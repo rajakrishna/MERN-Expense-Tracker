2e66931b9d81b851a702b56bc5738e7efde77eaa
Raja Krishna <rajakrishna214@gmail.com>
2020-07-03 21:49:06 -0500

    balance-tl-UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
 import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExp } from "./components/IncomeExp";
+import { TranscationList } from "./components/TranscationList";
 
 function App() {
 	return (
 		<div>
 			<Header />
+			<div className="container">
+				<Balance />
+				<IncomeExp />
+				<TranscationList />
+			</div>
 		</div>
 	);
 }
